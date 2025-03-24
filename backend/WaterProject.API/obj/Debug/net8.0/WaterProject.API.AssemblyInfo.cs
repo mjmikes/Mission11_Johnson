6d1d61b36c01779ccb3a07c59fec9a76d0863884
Micah <mjmikes@byu.edu>
2025-03-24 08:30:03 -0600

    hope this doesnt break anything

--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa8febf925d768675ecbd811f5366684dff61ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
